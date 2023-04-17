1a04a7d8e71e028fbbe85def65bd72440b4a6480
Zhesha <aleshko.info@gmail.com>
2023-04-17 23:28:11 +0400

    some task added

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  if(typeof s1 == "string" && typeof s2 == "string"){
+    let oneStroke = s1.split(''),
+      twoStroke = s2.split(''),
+      overlap = 0;
+
+      for(let i = 0; i < oneStroke.length; i++){
+        for(let k = 0; k < twoStroke.length; k++){
+          if(oneStroke[i] == twoStroke[k]){
+            overlap++;
+          }
+        }
+      }
+
+      return overlap;
+  }
 }
 
 module.exports = {
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,10 +12,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  var figures = "" + n;
+  var sum = 0;
+
+  for (var i = 0; i < figures.length; i++) 
+    sum += +figures[i]
+
+  return sum;
 }
+
 
 module.exports = {
   getSumOfDigits
